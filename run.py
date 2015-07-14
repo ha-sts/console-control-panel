d3de0ef76abe2bc09f17694ffd28711cb5779c1d
Daniel Williams <dwilliams@inovonics.com>
2015-07-14 09:35:55 -0600

    Adding a file.

---
+++ run.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+### IMPORTS ###
+import curses
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+
+### CLASSES ###
+
+### MAIN ###
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
